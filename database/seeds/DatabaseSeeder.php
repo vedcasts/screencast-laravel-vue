fc146ead9a96f59920d0755f14a7adc0a0e1e18c
Fábio Vedovelli <vedovelli@gmail.com>
2016-02-26 17:15:16 -0300

    Init + 50 usuarios usando Seed and Factory

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(UserTableSeeder::class);
+    }
+}
+
+class UserTableSeeder extends Seeder
+{
+    public function run()
+    {
+        factory(App\User::class, 50)->create()->each(function($u) {
+            $u->posts()->save(factory(App\Post::class)->make());
+        });
+    }
+}
